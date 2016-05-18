4a5b7b61343a4153e6627868b80f6240f69874f0
Michel Ge <strengthofthepen@gmail.com>
2016-05-18 14:00:18 -0500

    WOOP

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Personal Website</title>
+
+    <link href="bootstrap-3.3.6-dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="bootstrap-3.3.6-dist/css/bootstrap-theme.min.css" rel="stylesheet" />
+    <script src="jquery-1.12.1.js"></script>
+    <script src="bootstrap-3.3.6-dist/js/bootstrap.min.js"></script>
+    <script src="scripts/script.js"></script>
+    <link rel="stylesheet" type="text/css" href="styles/blogstyle.css" />
+
+</head>
+
+<body>
+
+</body>
+</html>
