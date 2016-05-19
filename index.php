20bed009663af19615f7856287502c8a4cff718d
Michel Ge <strengthofthepen@gmail.com>
2016-05-18 20:59:31 -0500

    ok..

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>Chapter One</title>
+<title>Chapter One...</title>
 
     <link href="bootstrap-3.3.6-dist/css/bootstrap.min.css" rel="stylesheet" />
     <link href="bootstrap-3.3.6-dist/css/bootstrap-theme.min.css" rel="stylesheet" />
