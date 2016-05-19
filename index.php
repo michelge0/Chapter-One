5ec42772a72f06922d67097ec07c4e03ff5c9b4f
Michel Ge <strengthofthepen@gmail.com>
2016-05-18 20:23:24 -0500

    init

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>Personal Website</title>
+<title>Chapter One</title>
 
     <link href="bootstrap-3.3.6-dist/css/bootstrap.min.css" rel="stylesheet" />
     <link href="bootstrap-3.3.6-dist/css/bootstrap-theme.min.css" rel="stylesheet" />
@@ -14,6 +14,8 @@
 </head>
 
 <body>
+
+
 
 </body>
 </html>
